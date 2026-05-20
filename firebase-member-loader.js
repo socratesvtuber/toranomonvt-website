c5e34a1641bff260a05d09ad2d0b7fe00f1784c4
socratesvtuber <socratesvtuber@gmail.com>
2026-05-20 10:50:00 +0900

    公開フラグの値がDBに格納する値とチェックする値で異なっていたのでプログラム側を修正

--- firebase-member-loader.js
+++ firebase-member-loader.js
@@ -652,8 +652,8 @@ if (window.firebaseMemberLoaderLoaded) {
     
     for (const id in this.allMembers) {
       const member = this.allMembers[id];
-      // Check if member should be displayed: "公開" means public, "非公開" means private
-      if (member.public_flag === '公開' || member.public_flag === true || member.public_flag === 'true') {
+      // Check if member should be displayed: "Web ページに公開する" means public
+      if (member.public_flag === 'Web ページに公開する' || member.public_flag === '公開' || member.public_flag === true || member.public_flag === 'true') {
         members.push({ id, ...member });
       }
     }
--- functions/api/google-form-handler/firebase-data-loader.js
+++ functions/api/google-form-handler/firebase-data-loader.js
@@ -117,8 +117,8 @@ const MemberDataLoader = {
     for (const id in submissions) {
       const member = submissions[id];
   
-      // Check if member should be displayed: "公開" means public, "非公開" means private
-      if (member.public_flag === '公開' || member.public_flag === true || member.public_flag === 'true') {
+      // Check if member should be displayed: "Web ページに公開する" means public
+      if (member.public_flag === 'Web ページに公開する' || member.public_flag === '公開' || member.public_flag === true || member.public_flag === 'true') {
         publicMembers.push({
           id,
           ...member
